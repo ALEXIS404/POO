a1d80b638975d362d3bb946606b3a9e1f7bc1738
ALEXIS404 <58710573+ALEXIS404@users.noreply.github.com>
2019-12-13 08:56:25 -0800

    Add files via upload

---
+++ src/Persona.java
@@ -0,0 +1,25 @@
+public class Persona {
+
+    //Atributos
+    private String nombre;
+    private int edad;
+    private String cedula;
+    private char sexo;
+    private double peso;
+    private double altura;
+
+    
+    //Contructores
+    public Persona(String _nombre, int _edad, String _cedula, char _sexo, double _peso, double _altura) {
+
+        this.nombre=nombre;
+        this.edad=edad;
+        this.cedula=cedula;
+        this.sexo=sexo;
+        this.peso=peso;
+        this.altura=altura;
+
+    }
+}
+
+
